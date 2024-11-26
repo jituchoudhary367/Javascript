6c2cc9880721f75f28a936f4534e25adf080ba32
Jitendra Chaudhary <146646530+jituchoudhary367@users.noreply.github.com>
2024-11-26 18:13:00 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accontID = 2303031570028
+let accountEmail = "jitendrachoudhary@gmail.com"
+var accountPassword = "1233456"
+accountCity = "Jaipur"
+
+accountId = 2322492
+console.log(accountId);
